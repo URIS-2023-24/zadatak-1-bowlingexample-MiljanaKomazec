e28ab4ac9c1fca9fd5bbbd3cf8de4fa1de994241
MiljanaKomazec <miljanakomazec123@gmail.com>
2023-10-25 14:07:17 +0200

    Zadatak - Bowling Game

---
+++ BowlingGame/BowlingGame1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class BowlingGame1
+    {
+
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public int Score {
+            get {
+                var score = 0;
+                var rollIndex = 0;
+                for (var frame = 0; frame < 10; frame++) {
+                    if (rolls[rollIndex] == 10)
+                    {
+                        score += rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+                        rollIndex++;
+                    }
+                    else if (rolls[rollIndex] + rolls[rollIndex + 1] == 10)
+                    {
+                        score += rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+                        rollIndex += 2;
+                    }
+                    else
+                    {
+                        score += rolls[rollIndex] + rolls[rollIndex + 1];
+                        rollIndex += 2;
+                    }
+                }
+
+                return score;
+            
+            }
+        
+        
+        }
+        public void Roll(int pins) {
+            rolls[currentRoll++] = pins;
+        }
+    }
+}
